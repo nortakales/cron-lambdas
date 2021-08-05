9d98564d85839e8079d30f41c0f9e205613229fc
Nick Ortakales <nortakales@gmail.com>
2021-08-04 23:50:31 -0700

    Add permissions

--- src/lib/crons/autox-reminder.ts
+++ src/lib/crons/autox-reminder.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as iam from '@aws-cdk/aws-iam';
 import { Schedule, Rule } from '@aws-cdk/aws-events'
 import { LambdaFunction } from '@aws-cdk/aws-events-targets'
 
@@ -23,6 +24,12 @@ export class AutoxReminderCron extends cdk.Construct {
             }
         });
 
+        lambdaFunction.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['ses:SendEmail'],
+            resources: ['*'],
+            effect: iam.Effect.ALLOW,
+        }));
+
         const dynamoTable = new dynamodb.Table(this, 'AutoxReminderDynamoTable', {
             partitionKey: {
                 name: 'url',
@@ -33,12 +40,14 @@ export class AutoxReminderCron extends cdk.Construct {
             tableName: 'autox_reminder_urls',
         });
 
-        dynamoTable.grantReadWriteData(lambdaFunction);
+        dynamoTable.grantWriteData(lambdaFunction);
+        dynamoTable.grantReadData(lambdaFunction);
 
         const schedule = new Rule(this, 'AutoxReminderSchedule', {
             schedule: Schedule.expression('rate(1 minute)'),
         });
 
         schedule.addTarget(new LambdaFunction(lambdaFunction));
+
     }
 }
