0b2fe61feb1c92dbaa649d16b152eb18978601d9
Nick Ortakales <nortakales@gmail.com>
2021-08-08 21:26:42 -0700

    Moved some config to a json file

--- src/lib/crons/autox-reminder.ts
+++ src/lib/crons/autox-reminder.ts
@@ -4,10 +4,9 @@ import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as iam from '@aws-cdk/aws-iam';
 import { Schedule, Rule } from '@aws-cdk/aws-events'
 import { LambdaFunction } from '@aws-cdk/aws-events-targets'
+import * as config from '../../config/config.json'
 
 export class AutoxReminderCron extends cdk.Construct {
-
-    private tableName = 'autox_reminder_urls';
 
     constructor(scope: cdk.Construct, id: string) {
         super(scope, id);
@@ -18,17 +17,10 @@ export class AutoxReminderCron extends cdk.Construct {
             code: lambda.Code.fromAsset('lambda'),
             handler: 'autox-reminder.handler',
             environment: {
-                emailList: [
-                    'nortakales@gmail.com',
-                    'kurt@hammondjp.com',
-                    'adamsdb@outlook.com',
-                    'michelegraaff@outlook.com',
-                    'sungcampbell@gmail.com',
-                    'nathan.olotoa@hotmail.com'
-                ].join(','),
-                from: 'nortakales@gmail.com',
-                subject: 'Evergreen Autox Alert',
-                tableName: this.tableName
+                emailList: config.autoxReminder.emailList.join(','),
+                from: config.autoxReminder.fromEmail,
+                subject: config.autoxReminder.emailSubject,
+                tableName: config.autoxReminder.dynamoTableName
             }
         });
 
@@ -45,13 +37,15 @@ export class AutoxReminderCron extends cdk.Construct {
             },
             billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
             removalPolicy: cdk.RemovalPolicy.DESTROY,
-            tableName: this.tableName
+            tableName: config.autoxReminder.dynamoTableName
         });
 
         dynamoTable.grantReadWriteData(lambdaFunction);
 
         const schedule = new Rule(this, 'AutoxReminderSchedule', {
-            schedule: Schedule.expression('rate(1 hour)'),
+            // Supports minute(s), hour(s), day(s):
+            // https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html#RateExpressions
+            schedule: Schedule.expression(config.autoxReminder.rate),
         });
 
         schedule.addTarget(new LambdaFunction(lambdaFunction));
