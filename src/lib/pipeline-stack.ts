ef2e3c54727b42ed05cd504ea03c82fe5e42eeb2
Nick Ortakales <nortakales@gmail.com>
2021-10-05 21:47:00 -0700

    Add pipeline notifications

--- src/lib/pipeline-stack.ts
+++ src/lib/pipeline-stack.ts
@@ -3,6 +3,10 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { DeployCronLambdaStage } from './deploy-cron-lambda-stage';
 import { SimpleSynthAction, CdkPipeline } from "@aws-cdk/pipelines";
+import * as notifications from '@aws-cdk/aws-codestarnotifications';
+import * as sns from '@aws-cdk/aws-sns';
+import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
+import * as config from '../config/config.json'
 
 export class CDKPipelineStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -38,9 +42,30 @@ export class CDKPipelineStack extends cdk.Stack {
             })
         });
 
-        // TODO A Lambda monitor to monitor for errors specifically for this
-
         const deploy = new DeployCronLambdaStage(this, 'DeployCronLambdaStage');
         pipeline.addApplicationStage(deploy);
+
+
+        const pipelineTopic = new sns.Topic(this, 'CronLambdaPipelineTopic', {
+            topicName: 'CronLambdaPipelineTopic',
+            displayName: 'CronLambdaPipelineTopic'
+        });
+        pipelineTopic.addSubscription(new subscriptions.EmailSubscription(config.base.infrastructureAlertEmail));
+
+        new notifications.NotificationRule(this, 'NotificationRule', {
+            source: pipeline.codePipeline,
+            events: [
+                'codepipeline-pipeline-action-execution-failed',
+                'codepipeline-pipeline-stage-execution-failed',
+                'codepipeline-pipeline-pipeline-execution-failed',
+                'codepipeline-pipeline-manual-approval-failed',
+                'codepipeline-pipeline-manual-approval-needed',
+
+                'codepipeline-pipeline-pipeline-execution-succeeded'
+            ],
+            targets: [
+                pipelineTopic
+            ],
+        });
     }
 }
