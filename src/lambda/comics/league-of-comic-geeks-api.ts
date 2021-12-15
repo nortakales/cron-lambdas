884076fd631b09e90a9efc337f8533b3376eb52b
Nick Ortakales <nortakales@gmail.com>
2021-12-14 20:41:17 -0800

    Test

--- src/lambda/comics/league-of-comic-geeks-api.ts
+++ src/lambda/comics/league-of-comic-geeks-api.ts
@@ -7,9 +7,9 @@ const baseComicUrl = "https://leagueofcomicgeeks.com";
 export async function getNewComics() {
 
     const html = await httpsGet("https://leagueofcomicgeeks.com/comics/new-comics", userAgent);
-    const root = dom.parse(html);
+    const rootzzz = dom.parse(html);
 
-    const comicList = root.querySelectorAll("#comic-list-issues > li");
+    const comicList = rootzzz.querySelectorAll("#comic-list-issues > li");
 
     let newComics: Comic[] = [];
 
