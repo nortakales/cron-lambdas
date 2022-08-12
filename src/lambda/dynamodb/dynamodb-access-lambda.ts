8ed1b7550da9e28e6afda82419573be71a27dee5
Nick Ortakales <nortakales@gmail.com>
2022-08-12 00:25:36 -0700

    Remove test

--- src/lambda/dynamodb/dynamodb-access-lambda.ts
+++ src/lambda/dynamodb/dynamodb-access-lambda.ts
@@ -124,146 +124,146 @@ async function del(event: any) {
 }
 
 // Uncomment this to call locally
-exports.handler({
-    "resource": "/",
-    "path": "/",
-    "httpMethod": "GET",
-    "headers": {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "CloudFront-Forwarded-Proto": "https",
-        "CloudFront-Is-Desktop-Viewer": "true",
-        "CloudFront-Is-Mobile-Viewer": "false",
-        "CloudFront-Is-SmartTV-Viewer": "false",
-        "CloudFront-Is-Tablet-Viewer": "false",
-        "CloudFront-Viewer-ASN": "7922",
-        "CloudFront-Viewer-Country": "US",
-        "Host": "2sc4ccorf3.execute-api.us-west-2.amazonaws.com",
-        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-        "Via": "1.1 c21a0d27ceec21e266c9f962d0349438.cloudfront.net (CloudFront)",
-        "X-Amz-Cf-Id": "A5gDtBFT_MDfviy_v6YO73NpbuTN1gselJc5cd9TfSrr0LY5O94p3w==",
-        "X-Amzn-Trace-Id": "Root=1-62f5fba2-7657b98c2f80b1c02aab0731",
-        "X-Forwarded-For": "73.169.243.70, 15.158.4.82",
-        "X-Forwarded-Port": "443",
-        "X-Forwarded-Proto": "https"
-    },
-    "multiValueHeaders": {
-        "Accept": [
-            "*/*"
-        ],
-        "Accept-Encoding": [
-            "gzip, deflate, br"
-        ],
-        "CloudFront-Forwarded-Proto": [
-            "https"
-        ],
-        "CloudFront-Is-Desktop-Viewer": [
-            "true"
-        ],
-        "CloudFront-Is-Mobile-Viewer": [
-            "false"
-        ],
-        "CloudFront-Is-SmartTV-Viewer": [
-            "false"
-        ],
-        "CloudFront-Is-Tablet-Viewer": [
-            "false"
-        ],
-        "CloudFront-Viewer-ASN": [
-            "7922"
-        ],
-        "CloudFront-Viewer-Country": [
-            "US"
-        ],
-        "Host": [
-            "2sc4ccorf3.execute-api.us-west-2.amazonaws.com"
-        ],
-        "User-Agent": [
-            "Thunder Client (https://www.thunderclient.com)"
-        ],
-        "Via": [
-            "1.1 c21a0d27ceec21e266c9f962d0349438.cloudfront.net (CloudFront)"
-        ],
-        "X-Amz-Cf-Id": [
-            "A5gDtBFT_MDfviy_v6YO73NpbuTN1gselJc5cd9TfSrr0LY5O94p3w=="
-        ],
-        "X-Amzn-Trace-Id": [
-            "Root=1-62f5fba2-7657b98c2f80b1c02aab0731"
-        ],
-        "X-Forwarded-For": [
-            "73.169.243.70, 15.158.4.82"
-        ],
-        "X-Forwarded-Port": [
-            "443"
-        ],
-        "X-Forwarded-Proto": [
-            "https"
-        ]
-    },
-    "queryStringParameters": {
-        "hashKey": "republic-fighter-tank-75342",
-        "hashKeyName": "urlKey",
-        "indexName": "urlKey-index",
-        "operation": "QUERY",
-        "rangeKey": "LEGO",
-        "rangeKeyName": "website",
-        "table": "products"
-    },
-    "multiValueQueryStringParameters": {
-        "hashKey": [
-            "republic-fighter-tank-75342"
-        ],
-        "hashKeyName": [
-            "urlKey"
-        ],
-        "indexName": [
-            "urlKey-index"
-        ],
-        "operation": [
-            "QUERY"
-        ],
-        "rangeKey": [
-            "LEGO"
-        ],
-        "rangeKeyName": [
-            "website"
-        ],
-        "table": [
-            "products"
-        ]
-    },
-    "pathParameters": null,
-    "stageVariables": null,
-    "requestContext": {
-        "resourceId": "aer14gvddj",
-        "resourcePath": "/",
-        "httpMethod": "GET",
-        "extendedRequestId": "WvRBdFsrvHcFgpg=",
-        "requestTime": "12/Aug/2022:07:05:06 +0000",
-        "path": "/prod",
-        "accountId": "787068200846",
-        "protocol": "HTTP/1.1",
-        "stage": "prod",
-        "domainPrefix": "2sc4ccorf3",
-        "requestTimeEpoch": 1660287906778,
-        "requestId": "7f4a1a04-41f8-41c4-b22b-92a5c7bc2aeb",
-        "identity": {
-            "cognitoIdentityPoolId": null,
-            "accountId": null,
-            "cognitoIdentityId": null,
-            "caller": null,
-            "sourceIp": "73.169.243.70",
-            "principalOrgId": null,
-            "accessKey": null,
-            "cognitoAuthenticationType": null,
-            "cognitoAuthenticationProvider": null,
-            "userArn": null,
-            "userAgent": "Thunder Client (https://www.thunderclient.com)",
-            "user": null
-        },
-        "domainName": "2sc4ccorf3.execute-api.us-west-2.amazonaws.com",
-        "apiId": "2sc4ccorf3"
-    },
-    "body": null,
-    "isBase64Encoded": false
-});
+// exports.handler({
+//     "resource": "/",
+//     "path": "/",
+//     "httpMethod": "GET",
+//     "headers": {
+//         "Accept": "*/*",
+//         "Accept-Encoding": "gzip, deflate, br",
+//         "CloudFront-Forwarded-Proto": "https",
+//         "CloudFront-Is-Desktop-Viewer": "true",
+//         "CloudFront-Is-Mobile-Viewer": "false",
+//         "CloudFront-Is-SmartTV-Viewer": "false",
+//         "CloudFront-Is-Tablet-Viewer": "false",
+//         "CloudFront-Viewer-ASN": "7922",
+//         "CloudFront-Viewer-Country": "US",
+//         "Host": "2sc4ccorf3.execute-api.us-west-2.amazonaws.com",
+//         "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+//         "Via": "1.1 c21a0d27ceec21e266c9f962d0349438.cloudfront.net (CloudFront)",
+//         "X-Amz-Cf-Id": "A5gDtBFT_MDfviy_v6YO73NpbuTN1gselJc5cd9TfSrr0LY5O94p3w==",
+//         "X-Amzn-Trace-Id": "Root=1-62f5fba2-7657b98c2f80b1c02aab0731",
+//         "X-Forwarded-For": "73.169.243.70, 15.158.4.82",
+//         "X-Forwarded-Port": "443",
+//         "X-Forwarded-Proto": "https"
+//     },
+//     "multiValueHeaders": {
+//         "Accept": [
+//             "*/*"
+//         ],
+//         "Accept-Encoding": [
+//             "gzip, deflate, br"
+//         ],
+//         "CloudFront-Forwarded-Proto": [
+//             "https"
+//         ],
+//         "CloudFront-Is-Desktop-Viewer": [
+//             "true"
+//         ],
+//         "CloudFront-Is-Mobile-Viewer": [
+//             "false"
+//         ],
+//         "CloudFront-Is-SmartTV-Viewer": [
+//             "false"
+//         ],
+//         "CloudFront-Is-Tablet-Viewer": [
+//             "false"
+//         ],
+//         "CloudFront-Viewer-ASN": [
+//             "7922"
+//         ],
+//         "CloudFront-Viewer-Country": [
+//             "US"
+//         ],
+//         "Host": [
+//             "2sc4ccorf3.execute-api.us-west-2.amazonaws.com"
+//         ],
+//         "User-Agent": [
+//             "Thunder Client (https://www.thunderclient.com)"
+//         ],
+//         "Via": [
+//             "1.1 c21a0d27ceec21e266c9f962d0349438.cloudfront.net (CloudFront)"
+//         ],
+//         "X-Amz-Cf-Id": [
+//             "A5gDtBFT_MDfviy_v6YO73NpbuTN1gselJc5cd9TfSrr0LY5O94p3w=="
+//         ],
+//         "X-Amzn-Trace-Id": [
+//             "Root=1-62f5fba2-7657b98c2f80b1c02aab0731"
+//         ],
+//         "X-Forwarded-For": [
+//             "73.169.243.70, 15.158.4.82"
+//         ],
+//         "X-Forwarded-Port": [
+//             "443"
+//         ],
+//         "X-Forwarded-Proto": [
+//             "https"
+//         ]
+//     },
+//     "queryStringParameters": {
+//         "hashKey": "republic-fighter-tank-75342",
+//         "hashKeyName": "urlKey",
+//         "indexName": "urlKey-index",
+//         "operation": "QUERY",
+//         "rangeKey": "LEGO",
+//         "rangeKeyName": "website",
+//         "table": "products"
+//     },
+//     "multiValueQueryStringParameters": {
+//         "hashKey": [
+//             "republic-fighter-tank-75342"
+//         ],
+//         "hashKeyName": [
+//             "urlKey"
+//         ],
+//         "indexName": [
+//             "urlKey-index"
+//         ],
+//         "operation": [
+//             "QUERY"
+//         ],
+//         "rangeKey": [
+//             "LEGO"
+//         ],
+//         "rangeKeyName": [
+//             "website"
+//         ],
+//         "table": [
+//             "products"
+//         ]
+//     },
+//     "pathParameters": null,
+//     "stageVariables": null,
+//     "requestContext": {
+//         "resourceId": "aer14gvddj",
+//         "resourcePath": "/",
+//         "httpMethod": "GET",
+//         "extendedRequestId": "WvRBdFsrvHcFgpg=",
+//         "requestTime": "12/Aug/2022:07:05:06 +0000",
+//         "path": "/prod",
+//         "accountId": "787068200846",
+//         "protocol": "HTTP/1.1",
+//         "stage": "prod",
+//         "domainPrefix": "2sc4ccorf3",
+//         "requestTimeEpoch": 1660287906778,
+//         "requestId": "7f4a1a04-41f8-41c4-b22b-92a5c7bc2aeb",
+//         "identity": {
+//             "cognitoIdentityPoolId": null,
+//             "accountId": null,
+//             "cognitoIdentityId": null,
+//             "caller": null,
+//             "sourceIp": "73.169.243.70",
+//             "principalOrgId": null,
+//             "accessKey": null,
+//             "cognitoAuthenticationType": null,
+//             "cognitoAuthenticationProvider": null,
+//             "userArn": null,
+//             "userAgent": "Thunder Client (https://www.thunderclient.com)",
+//             "user": null
+//         },
+//         "domainName": "2sc4ccorf3.execute-api.us-west-2.amazonaws.com",
+//         "apiId": "2sc4ccorf3"
+//     },
+//     "body": null,
+//     "isBase64Encoded": false
+// });
