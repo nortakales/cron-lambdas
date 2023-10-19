25ddab89e80fb2a5e2b08e71e502cf0b883843a1
Nick Ortakales <nortakales@gmail.com>
2023-10-18 19:20:44 -0700

    Another attempt at headers

--- src/lambda/http.ts
+++ src/lambda/http.ts
@@ -20,7 +20,8 @@ const RETRYABLE_CODES = [
 const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36';
 
 const DEFAULT_HEADERS = {
-    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    //'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9',
     'Referer': 'https://leagueofcomicgeeks.com/',
     'Upgrade-Insecure-Requests': '1'
