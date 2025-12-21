bf296ee2ef9259b4ca7e8401cfc9225502dd9047
Nick Ortakales <nortakales@gmail.com>
2025-12-20 20:12:15 -0800

    Try with no audio

--- src/lambda/alexa/handlers/control-device-intent-handler.ts
+++ src/lambda/alexa/handlers/control-device-intent-handler.ts
@@ -37,7 +37,8 @@ export async function handleControlDeviceIntent(event: any): Promise<string> {
         try {
             await sendSwitchBotCommand(canonicalDevice, canonicalCommand, repeat);
 
-            let successMessage = '<audio src="https://cron-lambdas-public-bucket.s3.us-west-2.amazonaws.com/audio/confirmation.mp3"/>'
+            // let successMessage = '<audio src="https://cron-lambdas-public-bucket.s3.us-west-2.amazonaws.com/audio/confirmation.mp3"/>'
+            let successMessage = 'Done'
             // 1 in 10 chance to add this message
             if (Math.random() < 0.1) successMessage += ' Nick is awesome!';
             return successMessage;
